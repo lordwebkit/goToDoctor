16b4774dd8d7dc7e08f431c560397dea6c667ad8
Kashirskiy Nikita <lordwebkit@gmail.com>
2024-05-22 19:06:13 +0300

    Вынес базовый bg стиль, добавил стили начертания шрифта

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <div class="app-wrapper font-sans">
+  <div class="font-sans">
     <RouterView />
   </div>
 </template>
 
 <script setup></script>
-<style scoped lang="scss">
-.app-wrapper {
-  background: url('./assets//image/main_header_bg.png') no-repeat;
-  min-height: 100vh;
-  min-width: 100vw;
-}
-</style>
